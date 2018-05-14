60cf56efc82f36ea73b4b7053f86f662ea5a68bd
Maxime Roccisano <maxime.roccisano@campus-numerique-in-the-alps.com>
2018-05-14 14:41:35 +0200

    first commit with HTML ready for auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Remédiation PHP
                 </div>
 
                 <div class="links">
